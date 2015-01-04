022d3bb6b14f61a59184a0082b00a41a1d8c90ea
seafoam6 <steve@stevebarman.com>
2015-01-04 12:04:12 -0500

    removes thumbnail link

--- single.php
+++ single.php
@@ -11,9 +11,9 @@
 
 			<!-- post thumbnail -->
 			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+				
 					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
+				
 			<?php endif; ?>
 			<!-- /post thumbnail -->
 
